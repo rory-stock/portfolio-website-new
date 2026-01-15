1334b9e53bff8e22debb6fa6e9a53e87225d1678
Rory <rorystock06@gmail.com>
2026-01-16 00:34:40 +1300

    build: update wrangler

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './server/db/schema.ts',
-  out: './server/db/migrations',
-  dialect: 'sqlite',
-})
+  schema: "./server/db/schema.ts",
+  out: "./server/db/migrations",
+  dialect: "sqlite",
+  driver: "d1-http",
+});
